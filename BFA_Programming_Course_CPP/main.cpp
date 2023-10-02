713ff4e8a9d3665b13fca9c01a5bcc4870b7a98a
Minuszh <haozheng.minus@gmail.com>
2023-10-02 17:38:03 +0800

    Initial Commit

---
+++ BFA_Programming_Course_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BFA_Programming_Course_CPP
+//
+//  Created by Minus on 2023/10/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
