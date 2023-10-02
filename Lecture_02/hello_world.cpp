f24815d187e8bf8958c426fd537b71aac1acbba6
Minuszh <haozheng.minus@gmail.com>
2023-10-02 17:55:57 +0800

    Test

---
+++ Lecture_02/hello_world.cpp
@@ -0,0 +1,18 @@
+//
+//  hello_world.cpp
+//  BFA_Programming_Course_CPP
+//
+//  Created by Minus on 2023/10/2.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "bye world" << endl;
+
+    return 0;
+
+}
