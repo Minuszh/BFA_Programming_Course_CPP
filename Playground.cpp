588f87cffeb9d6a48022188f780ba8ceb5d216df
Minuszh <haozheng.minus@gmail.com>
2023-10-18 14:56:54 +0800

    Add Lecture_04

---
+++ Playground.cpp
@@ -0,0 +1,22 @@
+//
+//  Playground.cpp
+//  BFA_Programming_Course_CPP
+//
+//  Created by Minus on 2023/10/8.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = 3;
+
+    cout << !a << endl;
+    
+    return 0;
+    
+}
+
+// bye bye
