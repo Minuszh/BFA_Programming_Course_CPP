023ad9e1391cd51b7d1c65d50aa6a6b9d6dd4213
Minuszh <haozheng.minus@gmail.com>
2023-11-10 10:47:41 +0800

    Complete 04_4

--- BFA_Programming_Course_CPP/Lecture_06/04_3_CalculateLogarithm.cpp
+++ BFA_Programming_Course_CPP/Lecture_06/04_3_CalculateLogarithm.cpp
@@ -8,4 +8,34 @@
 //
 //  Created by Minus on 2023/11/10.
 //
+//  Status:Wrong Answer
+//
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cin >> a >> b;
+    
+    double logAB = log(a)/log(b);
+    
+    int x=1;
+    
+    while(x<=20){
+        
+        x++;
+        
+        if( (x<=logAB && (x+1)>logAB) || (pow(a, x)<=b && b<=pow(a, x+1)) )
+            break;
+        
+    }
+    
+    cout << x ;
+    
+    return 0;
+    
+}
--- BFA_Programming_Course_CPP/Lecture_06/04_4_CaseConversion.cpp
+++ BFA_Programming_Course_CPP/Lecture_06/04_4_CaseConversion.cpp
@@ -2,7 +2,23 @@
 //  04_4_CaseConversion.cpp
 //  BFA_Programming_Course_CPP
 //
+//  http://bailian.openjudge.cn/practice/2689
+//
+//  大小写字母转换
+//
 //  Created by Minus on 2023/11/10.
 //
+//  Status:
+//
 
-#include "04_4_CaseConversion.hpp"
+#include <iostream>
+#include <cmath>
+int main(){
+
+    string beforeConv, afterConv;
+        
+        
+    return 0;
+
+    
+}
